f053a10cece6f87cb1caee127abf229a5bcae54c
vivek <vivekkumar16777@gmail.com>
2025-07-26 01:15:34 +0530

    added css and modified todocomp

--- src/assets/Todocompo.jsx
+++ src/assets/Todocompo.jsx
@@ -1,52 +1,74 @@
-import react, {useState} from 'react';
+import React, { useState } from 'react';
+import '../App.css';
 
-function Todocompo(){
-    
-    const [task , setTask] = useState(["play cricket"]);
-    const [newtask, setNewTask] = useState("");
+function Todocompo() {
+  const [task, setTask] = useState(["play cricket"]);
+  const [newtask, setNewTask] = useState("");
 
-    function addNewTask(event){
-        setNewTask(event.target.value);
+  function addNewTask(event) {
+    setNewTask(event.target.value);
+  }
+
+  function addTask() {
+    if (newtask.trim() !== "") {
+      setTask(t => [...t, newtask]);
+      setNewTask(""); 
     }
+  }
 
-    function addTask(){
-        if(newtask.trim() !== ""){
-            setTask(t=>[...t,newtask]);
-        }
-    }
+  function deleteTask(index) {
+    const updatedtask = task.filter((_, i) => i !== index);
+    setTask(updatedtask);
+  }
 
-    function deleteTask(index){
-        const updatedtask = task.filter((_,i) => i!==index);
-        setTask(updatedtask);
+  function upTask(index) {
+    const updatedtask = [...task];
+    if (index > 0) {
+      [updatedtask[index], updatedtask[index - 1]] = [updatedtask[index - 1], updatedtask[index]];
+      setTask(updatedtask);
     }
+  }
 
-    function upTask(index){
-        const updatedtask= [...task];
-        if(index > 0){
-            [updatedtask[index],updatedtask[index-1]]=[updatedtask[index-1],updatedtask[index]];
-            setTask(updatedtask);
-        }
+  function downTask(index) {
+    const updatedtask = [...task];
+    if (index < updatedtask.length - 1) {
+      [updatedtask[index + 1], updatedtask[index]] = [updatedtask[index], updatedtask[index + 1]];
+      setTask(updatedtask);
     }
-    
-    function downTask(index){
-        const updatedtask= [...task];
-        if(index < updatedtask.length-1){
-            [updatedtask[index+1],updatedtask[index]]=[updatedtask[index],updatedtask[index+1]];
-            setTask(updatedtask);
-        }
-    }
+  }
 
-    return(
+  return (
     <>
-    <div className='todolist'>
-        <h1>To-Do-List</h1>  
-    </div>
-    <input type="text" value={newtask} placeholder='enter a task...' onChange={addNewTask}></input>
-    <button  onClick={addTask}>add task</button>
-    <ol>
-        {task.map((t,index)=> <li key={index}><span> {t} </span> <button onClick = { () => deleteTask(index)}>delete</button> <button onClick={()=>upTask(index)}>move up</button> <button onClick={()=>downTask(index)}>move down</button></li>)}
-    </ol>
-    </> 
-    )
+      <div className="todolist">
+        <h1 className="todo-header">To-Do-List</h1>
+        <div className="search">
+          <input
+            className="todo-input"
+            type="text"
+            value={newtask}
+            placeholder="enter a task..."
+            onChange={addNewTask}
+          />
+          <button className="add-button" onClick={addTask}>add task</button>
+        </div>
+
+        <div className="todo-list">
+          <ol>
+            {task.map((t, index) => (
+              <li className="todo-item" key={index}>
+                <span className="todo-work">{t}</span>
+                <div className="button-group">
+                  <button className="delete" onClick={() => deleteTask(index)}>delete</button>
+                  <button onClick={() => upTask(index)}>move up</button>
+                  <button onClick={() => downTask(index)}>move down</button>
+                </div>
+              </li>
+            ))}
+          </ol>
+        </div>
+      </div>
+    </>
+  );
 }
+
 export default Todocompo;
